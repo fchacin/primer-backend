f2161a8c7de3a742117fe2055da1f0efa968d116
fchacin <franciscochacin96@gmail.com>
2024-01-16 11:49:42 -0400

    despliegue de api fl0

---
+++ schemas/movies.js
@@ -0,0 +1,27 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({ message: 'El poster debe ser un URL valido' }),
+  genre: z.array(z.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']), { required_error: 'Movie genre is required', invalid_type_error: 'Movie genre must be an array of enum Genre' })
+})
+
+function validarMovie (input) {
+  return movieSchema.safeParse(input)
+}
+
+function validarPartialmovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validarMovie,
+  validarPartialmovie
+}
